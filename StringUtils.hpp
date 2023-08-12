0d566c26104c659dbc78f88aba5d818b5865766f
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-08-12 20:52:57 +0530

    Juuuuuust a new line. ...And a spelling.

--- StringUtils.hpp
+++ StringUtils.hpp
@@ -5,31 +5,31 @@
 namespace StringUtils {
 
 #pragma region // `convertToString()` overloads.
-    template <typename PrintElementT>
-    std::string convertToString(const PrintElementT&);
+template <typename PrintElementT>
+std::string convertToString(const PrintElementT&);
 
-    template <typename PrintElementT>
-    std::vector<PrintElementT> convertToString(const std::vector<PrintElementT>&);
+template <typename PrintElementT>
+std::vector<PrintElementT> convertToString(const std::vector<PrintElementT>&);
 #pragma endregion
 
 #pragma region // `StringUtils::convertToLowercase()` overloads.
-    std::string convertToLowercase(std::string&);
+std::string convertToLowercase(std::string&);
 
-    std::vector<std::string>& convertToLowercase(const std::vector<char*>&);
+std::vector<std::string>& convertToLowercase(const std::vector<char*>&);
 
-    /**
-     * @param list the vector of strings to convert the strings of, to lowercase.
-     * @return The modified vector passed in.
-     */
-    std::vector<std::string>& convertToLowercase(const std::vector<std::string>&);
+/**
+ * @param list the vector of strings to convert the strings of, to lowercase.
+ * @return The modified vector passed in.
+ */
+std::vector<std::string>& convertToLowercase(const std::vector<std::string>&);
 
-    /**
-     * @return A new vector, after copyinf and converting all string elements in the argument one, to lowercase.
-     */
-    std::vector<std::string> convertCopyToLowercase(const std::vector<std::string>&);
+/**
+ * @return A new vector, after copying and converting all string elements in the argument one, to lowercase.
+ */
+std::vector<std::string> convertCopyToLowercase(const std::vector<std::string>&);
 #pragma endregion
 
-    std::vector<std::string> splitStringBySpaces(const std::string& p_str);
-    std::vector<std::string> splitStringByChar(const std::string& p_str, const char p_delimiter);
+std::vector<std::string> splitStringBySpaces(const std::string& p_str);
+std::vector<std::string> splitStringByChar(const std::string& p_str, const char p_delimiter);
 
 }
